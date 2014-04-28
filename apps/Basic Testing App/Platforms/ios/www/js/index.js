a8fdac29d3d733126e745fd83f38819294a5047a
steven-king <cujophoto@gmail.com>
2014-04-28 16:11:18 -0400

    fixing the events

--- apps/Basic Testing App/Platforms/ios/www/js/index.js
+++ apps/Basic Testing App/Platforms/ios/www/js/index.js
@@ -44,7 +44,7 @@ var app = {
             });
             
             function data(jsonp) {
-                console.log(jsonp);
+                //console.log(jsonp);
                 $.each( jsonp.posts.reverse(), function( i, post ) {
                     title = post.title;
                     content = post.content;
@@ -68,34 +68,35 @@ var app = {
     
     onDeviceReady: function() {
         console.log('deviceready');
-        $("#message").html("Device Read and listening...");
+        $("#message").html("Device Ready and listening...");
         var callback = app.receivedFrequency;
         window.pitchDetection.registerFrequency( "18000", callback );
-        window.pitchDetection.registerFrequency( "18100", callback );
+        //window.pitchDetection.registerFrequency( "18100", callback );
         window.pitchDetection.registerFrequency( "18200", callback );
-        window.pitchDetection.registerFrequency( "18300", callback );
+        //window.pitchDetection.registerFrequency( "18300", callback );
         window.pitchDetection.registerFrequency( "18400", callback );
-        window.pitchDetection.registerFrequency( "18500", callback );
+        //window.pitchDetection.registerFrequency( "18500", callback );
         window.pitchDetection.registerFrequency( "18600", callback );
-        window.pitchDetection.registerFrequency( "18700", callback );
+        //window.pitchDetection.registerFrequency( "18700", callback );
         window.pitchDetection.registerFrequency( "18800", callback );
+        //window.pitchDetection.registerFrequency( "18900", callback );
         window.pitchDetection.registerFrequency( "19000", callback );
-        window.pitchDetection.registerFrequency( "19100", callback );
+        //window.pitchDetection.registerFrequency( "19100", callback );
         window.pitchDetection.registerFrequency( "19200", callback );
-        window.pitchDetection.registerFrequency( "19300", callback );
+        //window.pitchDetection.registerFrequency( "19300", callback );
         window.pitchDetection.registerFrequency( "19400", callback );
-        window.pitchDetection.registerFrequency( "19500", callback );
+        //window.pitchDetection.registerFrequency( "19500", callback );
         window.pitchDetection.registerFrequency( "19600", callback );
-        window.pitchDetection.registerFrequency( "19700", callback );
+        //window.pitchDetection.registerFrequency( "19700", callback );
         window.pitchDetection.registerFrequency( "19800", callback );
         window.pitchDetection.registerFrequency( "20000", callback );
-        window.pitchDetection.registerFrequency( "20100", callback );
+        //window.pitchDetection.registerFrequency( "20100", callback );
         window.pitchDetection.registerFrequency( "20200", callback );
-        window.pitchDetection.registerFrequency( "20300", callback );
+        //window.pitchDetection.registerFrequency( "20300", callback );
         window.pitchDetection.registerFrequency( "20400", callback );
-        window.pitchDetection.registerFrequency( "20500", callback );
+        //window.pitchDetection.registerFrequency( "20500", callback );
         window.pitchDetection.registerFrequency( "20600", callback );
-        window.pitchDetection.registerFrequency( "20700", callback );
+        //window.pitchDetection.registerFrequency( "20700", callback );
         window.pitchDetection.registerFrequency( "20800", callback );
         window.pitchDetection.registerFrequency( "30000", callback );
         window.pitchDetection.startListener();
@@ -110,95 +111,44 @@ var app = {
         if (frequency == 18000) {
             chapter = 0;
         }
-        if (frequency == 18100) {
+        if (frequency == 18200) {
             chapter = 1;
         }
-        if (frequency == 18200) {
+        if (frequency == 18400) {
             chapter = 2;
         }
-        if (frequency == 18300) {
+        if (frequency == 18600) {
             chapter = 3;
         }
-        if (frequency == 18400) {
+        if (frequency == 18800) {
             chapter = 4;
         }
-        if (frequency == 18500) {
+        if (frequency == 19000) {
             chapter = 5;
         }
-        if (frequency == 18600) {
+        if (frequency == 19200) {
             chapter = 6;
         }
-        if (frequency == 18700) {
+        if (frequency == 19400) {
             chapter = 7;
         }
-        if (frequency == 18800) {
+        if (frequency == 19600) {
             chapter = 8;
         }
-        if (frequency == 18900) {
+        if (frequency == 19800) {
             chapter = 9;
         }
-        if (frequency == 19000) {
+        if (frequency == 20000) {
             chapter = 10;
         }
-        if (frequency == 19100) {
+        if (frequency == 20200) {
             chapter = 11;
         }
-        if (frequency == 19200) {
+        if (frequency == 20400) {
             chapter = 12;
         }
-        if (frequency == 19300) {
-            chapter = 13;
-        }
-        if (frequency == 19400) {
-            chapter = 14;
-        }
-        if (frequency == 19500) {
-            chapter = 15;
-        }
-        if (frequency == 19600) {
-            chapter = 16;
-        }
-        if (frequency == 19700) {
-            chapter = 17;
-        }
-        if (frequency == 19800) {
-            chapter = 18;
-        }
-        if (frequency == 19900) {
-            chapter = 19;
-        }
-        if (frequency == 20000) {
-            chapter = 20;
-        }
-        if (frequency == 21000) {
-            chapter = 21;
-        }
-        if (frequency == 20200) {
-            chapter = 22;
-        }
-        if (frequency == 20300) {
-            chapter = 23;
-        }
-        if (frequency == 20400) {
-            chapter = 24;
-        }
-        if (frequency == 20500) {
-            chapter = 25;
-        }
         if (frequency == 20600) {
-            chapter = 26;
-        }
-        if (frequency == 20700) {
-            chapter = 27;
-        }
-        if (frequency == 20800) {
-            chapter = 28;
-        }
-        if (frequency == 20900) {
-            chapter = 29;
-        }
-        if (frequency == 30000) {
-            chapter = 30;
+            chapter = 13;
         }
         $("#message").html("Freq Detected. Load Ch: " + chapter);
         $( "#filmSlider" ).attr( "style", "left:"+ (chapter*-100) +"%;" );
